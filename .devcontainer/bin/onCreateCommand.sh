7467aac1eccbcf9712fb0411aadde09921ea8ee5
wmcdonald404 <wmcdonald@gmail.com>
2024-10-16 11:07:20 +0000

    - Add oh-my-bash to the devcontainer.

--- .devcontainer/bin/onCreateCommand.sh
+++ .devcontainer/bin/onCreateCommand.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-dnf -y install git pytest
+dnf -y install git python3-pytest
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
