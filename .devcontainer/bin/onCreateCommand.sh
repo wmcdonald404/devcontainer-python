99857e348fa85880b1e78adeee933bd06090ad14
wmcdonald404 <wmcdonald@gmail.com>
2024-10-16 11:21:20 +0000

    - Switch to unattended oh-my-bash install.

--- .devcontainer/bin/onCreateCommand.sh
+++ .devcontainer/bin/onCreateCommand.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 dnf -y install git python3-pytest
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
